e75b94625600e8116a37690ad4399ec6f71d0cac
pc00 <pc00@pc00>
2023-05-16 16:09:21 +0900

    mapper 세팅 안한 버전. mapper 에서 가져오는 xml 내용이 비어있으면 오류 발생

---
+++ test/src/main/java/com/example/biz/board/impl/SchaduleDAO.java
@@ -0,0 +1,16 @@
+package com.example.biz.board.impl;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+
+
+
+public class SchaduleDAO {
+	
+	
+	private SqlSessionTemplate mybatis;
+
+	public void insertBoardSchadule() {
+		mybatis.insert("");
+	}
+}
---
+++ test/src/main/java/com/example/biz/board/impl/VacationDAO.java
@@ -0,0 +1,15 @@
+package com.example.biz.board.impl;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+
+
+public class VacationDAO {
+	
+	
+	private SqlSessionTemplate mybatis;
+
+	public void insertBoardVacation() {
+		mybatis.insert("");
+	}
+}
